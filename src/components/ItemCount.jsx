86e9445fa15514abcb42d4c64da1c32034f9592c
leogd10 <leonardoaprea@gmail.com>
2022-09-06 20:02:03 -0300

    cleaned unused code

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export const ItemCount = ({ stock, initial, onAdd }) => {
 
@@ -8,20 +8,20 @@ export const ItemCount = ({ stock, initial, onAdd }) => {
         setCount(count - 1);
     }
 
-    const increase = () =>{
+    const increase = () => {
         setCount(count + 1);
     }
 
     return (
-        <>
-            <div className="ItemCounter_Container">
-                <div className='Counter_Container'>
-                    <button className='btn-decrease' onClick={decrease} disabled={count <=1}>-</button>
-                    <span className='item-quantity'>{count}</span>
-                    <button className='btn-increase' onClick={increase} disabled={count >= stock}>+</button>
-                </div>
-                <button className="btn-addToCart" disabled={stock <= 0}  onClick={() => onAdd(count)}>Add to cart</button>
+
+        <div className="ItemCounter_Container">
+            <div className='Counter_Container'>
+                <button className='btn-decrease' onClick={decrease} disabled={count <= 1}>-</button>
+                <span className='item-quantity'>{count}</span>
+                <button className='btn-increase' onClick={increase} disabled={count >= stock}>+</button>
             </div>
-        </>
+            <button className="btn-addToCart" disabled={stock <= 0} onClick={() => onAdd(count)}>Add to cart</button>
+        </div>
+
     )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -38,5 +38,3 @@ export const ItemListContainer = () => {
 
     )
 }
-
-export default ItemListContainer
